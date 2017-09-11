feec6b0ca8d3e416523f681dbe35484176108e24
Safak <safakge@gmail.com>
2017-09-11 22:27:04 +0300

    Fixed a bug where the picker would crash when shown with maxMinutes < 60

--- TimeIntervalPicker/Classes/TimeIntervalPicker.swift
+++ TimeIntervalPicker/Classes/TimeIntervalPicker.swift
@@ -279,7 +279,7 @@ open class TimeIntervalPicker: UIView, UIPickerViewDelegate, UIPickerViewDataSou
                 self.pickerView?.selectRow(hour, inComponent: 0, animated: true)
                 self.pickerView?.selectRow(minute, inComponent: 1, animated: true)
             } else {
-                self.pickerView?.selectRow(minute, inComponent: 1, animated: true)
+                self.pickerView?.selectRow(minute, inComponent: numberOfComponents(in: pickerView!) == 1 ? 0 : 1, animated: true)
             }
         }
         
